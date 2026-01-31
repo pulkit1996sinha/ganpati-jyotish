f3b849d4abd02d47ecb955b22b052b890e69f65e
Pulkit Sinha <pulkitsinha@Pulkits-MacBook-Air.local>
2026-01-31 16:32:54 +0530

    migrate database to postgres

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,13 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./jyotish.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
-
-SessionLocal = sessionmaker(bind=engine, autoflush=False)
-Base = declarative_base()
